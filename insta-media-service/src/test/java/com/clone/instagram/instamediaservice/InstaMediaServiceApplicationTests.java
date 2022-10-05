e73eadaf60bb1e786e9890d60ef46cd0414d9b17
riazbls455 <riazbls455@gmail.com>
2022-10-05 06:59:29 +0530

    created insta-media-service

---
+++ insta-media-service/src/test/java/com/clone/instagram/instamediaservice/InstaMediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.instagram.instamediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaMediaServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
